985427326e1c778575594511880ee9ec0a0328a6
Conor Souhrada <ConorSouhrada@gmail.com>
2018-08-23 12:07:52 -0600

    location added

--- src/components/nowClimbing.js
+++ src/components/nowClimbing.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 import NavBar from './navbar';
 
 class NowClimbing extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      yourLocal: null,
+      boxAccess: false
+    }
+  }
 
   componentDidMount(){
     console.log(this.props.location.state.mountian)
   }
+
+
+  confirmSummit(){
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.setState({yourLocation: position})
+        console.log(this.state.yourLocation)
+        this.summitSuccess()
+      })
+    }
+
+  summitSuccess(){
+    const yourLat = Math.round(this.state.yourLocation.coords.latitude * 1000) / 1000
+    const yourLng = Math.round(this.state.yourLocation.coords.longitude * 1000) / 1000
+
+    const mtLat = Math.round(this.props.location.state.mountian.lat * 1000) / 1000
+    const mtLng = Math.round(this.props.location.state.mountian.lng * 1000) / 1000
+
+    if(yourLat === mtLat && yourLng === mtLng){
+      this.setState({boxAccess: true})
+    }
+  }
+
+
 
   render() {
     return (
@@ -18,9 +49,12 @@ class NowClimbing extends Component {
         <p>
           Elevation: {this.props.location.state.mountian.elevation} ft.
         </p>
-        <button>
+        <button onClick={() => this.confirmSummit()}>
           Confirm Summit
         </button>
+        <p> {this.state.yourLocation ? this.state.yourLocation.coords.latitude : ""} </p>
+        <p> {this.state.yourLocation ? this.state.yourLocation.coords.longitude : ""} </p>
+        <h1> {this.state.boxAccess ? "You Have Reached the SummitBox" : "Box Access Denied"} {} </h1>
         <NavBar />
       </div>
     );
